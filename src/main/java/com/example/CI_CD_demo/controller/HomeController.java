caecd3d6ecaa41454f28e908b15bb7a96938b656
DK1306 <kumardhinesh35419@gmail.com>
2026-06-29 13:45:46 +0530

    /hello API is added.....

---
+++ src/main/java/com/example/CI_CD_demo/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.example.CI_CD_demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/hello")
+    public String Greet(){
+        return "CI-CD DEMO WELCOME.....!!!";
+    }
+
+}
---
+++ src/test/java/com/example/CI_CD_demo/CiCdDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CI_CD_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
